6eb7b47ebcb3ff8cdcbf0d361252d12b4fbe7ba2
Brendan Buslig <ffbrendan@free.fr>
2021-11-22 22:36:35 +0100

    server.js + création des dossiers

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// Importer le package HTTP de node.js pour créer le serveur
+const http = require("http");
+
+// Importer l'application app.js
+const app = require("./app");
+
+// Paramètrage du port avec la méthode set de Express
+app.set("port", process.env.PORT || 3000);
+
+// Methode qui retourne une nouvelle instance de http.server
+const server = http.createServer(app);
+
+// Demarre le serveur HTTP et écoute les connexions
+server.listen(process.env.PORT || 3000);
+
