39b3fb66792ecc5550b71cded70c340603249269
Brendan Buslig <ffbrendan@free.fr>
2021-11-30 19:03:40 +0100

    modifier server + sauce

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,51 @@ const http = require("http");
 // Importer l'application app.js
 const app = require("./app");
 
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
 // Paramètrage du port avec la méthode set de Express
-app.set("port", process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
 // Methode qui retourne une nouvelle instance de http.server
 const server = http.createServer(app);
 
-// Demarre le serveur HTTP et écoute les connexions
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
+// Demarre le serveur HTTP et écoute les connexions
+server.listen(port);
