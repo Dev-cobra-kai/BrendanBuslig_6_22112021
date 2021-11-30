4ebead6db08c73838fdf2e19442e5648999d0b07
Brendan Buslig <ffbrendan@free.fr>
2021-11-30 22:28:00 +0100

    test du backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,7 +6,7 @@ const user = require('../models/user');
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
-            const user = new User({
+            const user = new user({
                 email: req.body.email,
                 password: hash
             });
