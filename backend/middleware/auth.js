1db1a2112e066d3088ace58217a9ed4d12ac89f4
Brendan Buslig <ffbrendan@free.fr>
2023-02-18 19:15:39 +0100

    Finition Projet

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@
 
 // Importer JsonWebToken
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+// require('dotenv').config();
 
 // Utilisateur authentifié = routes sécurisées
 module.exports = (req, res, next) => {
@@ -10,11 +10,14 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // Empêche d’usurper l’identité de l’utilisateur
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'User ID non valable';
-        } else {
+        req.auth = {
+            userId: userId
+        };
+        // if (req.body.userId && req.body.userId !== userId) {
+        //     throw 'User ID non valable';
+        // } else {
             next();
-        }
+        // }
     } catch {
         res.status(401).json({
             error: new Error('Requête non authentifiée!')
